8c960ae3ff9c877700cc189df9aa39d2f86cfefd
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-06-16 14:49:00 +0700

    Update lend_amount.py

--- lend_models/class_models/lend_amount.py
+++ lend_models/class_models/lend_amount.py
@@ -56,12 +56,12 @@ def add(sender, instance, created, **kwargs):
         #     current_instance=instance,
         #     current_amount=instance.amount
         # ).outlet()
-        LendingMoneyWithdraw(
-            is_return=instance.is_return,
-            is_withdraw=instance.is_withdraw,
-            pk=instance.account.id,
-            amount=instance.amount
-        ).withdraw_money()
+        # LendingMoneyWithdraw(
+        #     is_return=instance.is_return,
+        #     is_withdraw=instance.is_withdraw,
+        #     pk=instance.account.id,
+        #     amount=instance.amount
+        # ).withdraw_money()
 
 
 @receiver(pre_save, sender=LendAmount)
@@ -84,12 +84,14 @@ def update(sender, instance, **kwargs):
 @receiver(pre_delete, sender=LendAmount)
 def delete(sender, instance, using, **kwargs):
     old_value = LendAmount.objects.get(id=instance.id)
-    LendingMoneyReturn(
-        is_return=old_value.is_return,
-        is_withdraw=old_value.is_withdraw,
-        pk=old_value.account.id,
-        amount=old_value.amount
-    ).return_money()
+
+    # LendingMoneyReturn(
+    #     is_return=old_value.is_return,
+    #     is_withdraw=old_value.is_withdraw,
+    #     pk=old_value.account.id,
+    #     amount=old_value.amount
+    # ).return_money()
+
     # BalanceRefund(
     #     last_condition=old_value.is_return,
     #     last_instance=old_value,
